3965a013969b87a709b977e2b877584a41d171b8
bladerangel <bladerangel10@gmail.com>
2018-06-01 21:06:35 -0300

    remove ionic page from tabs

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage } from 'ionic-angular';
 
 import { FavoritesPage } from '../favorites/favorites';
 import { LibraryPage } from '../library/library';
 
-@IonicPage()
 @Component({
     selector: 'page-tabs',
     templateUrl: 'tabs.html',
