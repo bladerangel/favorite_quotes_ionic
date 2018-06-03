ddd2c511ff1d14ba66bf1b5eb880964aa8a0b8eb
bladerangel <bladerangel10@gmail.com>
2018-06-03 06:52:51 -0300

    update settings page

--- src/pages/seetings/settings.ts
+++ src/pages/seetings/settings.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Toggle } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -11,8 +11,8 @@ export class SettingsPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SettingsPage');
+  onToggle(toggle: Toggle) {
+    console.log(toggle);
   }
 
 }
