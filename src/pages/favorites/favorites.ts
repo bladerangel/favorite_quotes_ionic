b3dd32ea5417d55896b6ba73451cf2af2928440f
bladerangel <bladerangel10@gmail.com>
2018-06-03 04:11:15 -0300

    create view modal

--- src/pages/favorites/favorites.ts
+++ src/pages/favorites/favorites.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage } from 'ionic-angular';
+import { IonicPage, ModalController } from 'ionic-angular';
 
 import { Quote } from '../../data/quote.interface';
 import { QuotesProvider } from '../../providers/quotes/quotes';
@@ -14,9 +14,21 @@ export class FavoritesPage {
 
   quotes: Quote[];
 
-  constructor(private quotesProvider: QuotesProvider) { }
+  constructor(private quotesProvider: QuotesProvider, private modalCtrl: ModalController) { }
 
   ionViewWillEnter() {
     this.quotes = this.quotesProvider.getFavoriteQuotes();
+  }
+
+  onViewQuote(quote: Quote) {
+    const modal = this.modalCtrl.create('QuotePage', quote);
+    modal.onDidDismiss((remove: boolean) => {
+      if (remove) {
+        this.quotesProvider.removeQuoteFromFavorite(quote);
+        this.quotes = this.quotesProvider.getFavoriteQuotes();
+      }
+    });
+    modal.present();
+   
   }
 }
