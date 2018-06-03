e2d7c8115380739f17f506273b270d89c596d98d
bladerangel <bladerangel10@gmail.com>
2018-06-03 06:21:43 -0300

    config side menu

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,23 +1,12 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController } from 'ionic-angular';
 
-/**
- * Generated class for the TestePage tabs.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-tabs',
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
-
-  favoritesPageRoot = 'FavoritesPage'
-  libraryPageRoot = 'LibraryPage'
-
 
   constructor(public navCtrl: NavController) {}
 
